875a96a03f55560356ed0b315f9f795ef167cf06
chef-Cuisto <116522751+chef-Cuisto@users.noreply.github.com>
2023-03-11 17:42:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php include 'footnav/header.php'; ?> <!-- pour inclure le nav qui est fait sur le dossier footnav -->
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/style.css">
+    <title>Portfolio</title>
+</head>
+<body>
+<section class="page-content">
+    <header>
+        <h1>Bienvenue sur mon Portefolio</h1>
+        <p>Développeur web en alternance</p>
+        <a href="#"><button>Me connaitre</button></a>
+    </header>
+    <section class="presentation-image">
+        <img src="images/main.jpg" alt="">
+    </section>
+    <section class="projects-section">
+        <h1>Projets réalisés</h1>
+        <div class="projets">
+            <div class="projet">
+                <h4>Projet 1</h4>
+                <a href="https://translate.google.fr/?hl=fr&sl=fr&tl=es&text=si%20tu%20as%20cliqu%C3%A9%20ici%20c%27est%20que%20tu%20es%20un%20bg&op=translate"><img src="images/projets.jpg" alt=""></a>
+                <p class="description"> Lorem ipsum dolor sit 
+                    amet, consectetur
+                    adipisicing elit. Consequatur 
+                    iure numquam mollitia .</p>
+                <p class="date">2001 - 2003</p>
+            </div>
+            <div class="projet">
+                <h4>Projet 2</h4>
+                <img src="images/projets.jpg" alt="">
+                <p class="description"> Lorem ipsum dolor sit 
+                    amet, consectetur
+                    adipisicing elit. Consequatur 
+                    iure numquam mollitia .</p>
+                <p class="date">2001 - 2002</p>
+            </div>
+            <div class="projet">
+                <h4>Projet 3</h4>
+                <img src="images/projets.jpg" alt="">
+                <p class="description"> Lorem ipsum dolor sit 
+                    amet, consectetur
+                    adipisicing elit. Consequatur 
+                    iure numquam mollitia .t</p>
+                <p class="date">2003 - 2004</p>
+            </div>
+            <div class="projet">
+                <h4>Projet 4</h4>
+                <img src="images/projets.jpg" alt="">
+                <p class="description"> Lorem ipsum dolor sit 
+                    amet, consectetur
+                    adipisicing elit. Consequatur 
+                    iure numquam mollitia .</p>
+                <p class="date">2005 - 2006</p>
+            </div>
+            <div class="projet">
+                <h4>Projet 5</h4>
+                <img src="images/projets.jpg" alt="">
+                <p class="description"> Lorem ipsum dolor sit 
+                    amet, consectetur
+                    adipisicing elit. Consequatur 
+                    iure numquam mollitia .</p>
+                <p class="date">2007 - 2008</p>
+            </div>
+        </div>
+        </div>
+    </section>
+</section>
+<?php include 'footnav/footer.php'; ?> <!-- pour inclure le footer qui est fait sur le dossier footnav -->
+    
+</body>
+</html>
